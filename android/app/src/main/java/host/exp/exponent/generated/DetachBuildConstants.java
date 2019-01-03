78eac305add8c0224bda12cdce16312f88fdb824
mordochi <lilynnchi@gmail.com>
2019-01-03 11:35:50 +0800

    feat: build in android

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp92edeafd9ce64aed84620e02b9b24f5b://10.1.1.22:19000";
 
 }
